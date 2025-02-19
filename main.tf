8831bbde29f07ca315591d8029cac2420d576871
hindusree444 <134682745+hindusree444@users.noreply.github.com>
2025-02-19 20:38:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,19 @@
 terraform {
   backend "gcs" {
-    bucket      = "migrationbucket1"   # GCS bucket name
-    prefix      = "terraform/state"                # Path to store the state file within the bucket (optional)
-    credentials = "C:/Users/hr352/Downloads/statemigration-152f2dfa7870.json" # Path to your service account credentials file
+    bucket      = "migrationbucket1"   
+    prefix      = "terraform/state"               
   }
 }
 
 provider "google" {
   project = "statemigration"
   region  = "us-central1"
-  credentials = "C:/Users/hr352/Downloads/statemigration-152f2dfa7870.json"
 }
 
 resource "google_compute_instance" "vm_instance" {
   name         = "my-vm"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"  # Specify the zone where the instance will be created
-
+  zone         = "us-central1-a"  
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -27,7 +24,7 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     network = "default"
     access_config {
-      # This enables an external IP
+    
     }
   }
 }
